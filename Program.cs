f7af7ee3253105a68d5266e18871fc2cb82f5cbd
Lera <badasyan.lera@mail.ru>
2022-08-14 22:04:57 +0300

    added decision of 10 task

--- Program.cs
+++ Program.cs
@@ -4,4 +4,6 @@ Console.Clear();
 
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-
+int threeDigitalnumber = new Random().Next(100, 1000);
+int secondnumber = threeDigitalnumber / 10 % 10;
+Console.WriteLine($"second number {threeDigitalnumber} -> {secondnumber}");
