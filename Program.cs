7e272e3a8f7c5e134e83590796ad88eecebd1e39
Lera <badasyan.lera@mail.ru>
2022-08-14 22:24:42 +0300

    added desicion 13 tsk

--- Program.cs
+++ Program.cs
@@ -4,6 +4,17 @@ Console.Clear();
 
 // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int threeDigitalnumber = new Random().Next(100, 1000);
-int secondnumber = threeDigitalnumber / 10 % 10;
-Console.WriteLine($"second number {threeDigitalnumber} -> {secondnumber}");
+// int threeDigitalnumber = new Random().Next(100, 1000);
+// int secondnumber = threeDigitalnumber / 10 % 10;
+// Console.WriteLine($"second number {threeDigitalnumber} -> {secondnumber}");
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// int number = new Random().Next(1, 1000);
+// if(number<100)
+// {
+//     Console.WriteLine($"Nonexistent number: {number}");
+//     return;
+// }
+// int thirdnumber = (number%100)%10;
+// Console.WriteLine($"third number {number} -> {thirdnumber}");
